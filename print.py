49f2cdd1121305338a9a2cdbfde3a573d40f7c5e
mahak <kaushikikes@gmail.com>
2026-04-05 22:07:56 +0530

    learn built in function print

---
+++ print.py
@@ -0,0 +1,14 @@
+print("Hello, World!")
+print(True,5,False)
+print(1e309)
+print(type(1.23))#float
+print(type(234466765467))#integer
+print(type(True))#boolean
+print(type(None))#nonetype
+print(type(4+5j))#complex
+print(type("Hello, World!"))#string
+print(type('''hello'''))#string
+print([1,2,3,5,6,7])#list uses [] => square braces
+print((1,2,3,4,5,6,7))#tuple uses () => parenthesis
+print({1,2,3,4,5,6,7,7,7})#sets uses {} => curly braces
+print({"name":"palak","age":19})#dict
