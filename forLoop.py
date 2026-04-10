7bf1f9fc881938f2a3b86b0b538761d76670b001
mahak <kaushikikes@gmail.com>
2026-04-10 12:26:17 +0530

    strings

--- forLoop.py
+++ forLoop.py
@@ -6,3 +6,10 @@
 
 for i in [1,2,3,4,5,6]:
     print(i) 
+
+#nested loops
+
+for i in range(0,5):
+    for j in range(0,i):
+        print("*",end=" ")
+    print()
---
+++ string.py
@@ -0,0 +1,33 @@
+print("this is a book.\n now read \t fgc")
+
+#operations in string
+
+#1.concatenation
+
+str1 = "makhanlal"
+str2 = "chaturedi"
+
+print(str1+str2)
+
+#2.len()
+
+length = len(str1)
+print(length)
+
+#3.indexing
+
+print(str1[0],str1[1])
+#str1[2] = 'r'we cannot assign new item 
+
+
+#slicing
+
+print(str1[0:5]) #starting index inclusive but ending index exclusive
+
+#negative indexing in python 
+# a  p  p  l  e
+#-5 -4 -3 -2 -1
+
+str ="apple"
+print(str[-3:-1])
+
