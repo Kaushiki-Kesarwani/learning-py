0bb3e9a113941058f574d08bebefadb5b4eab9f1
mahak <kaushikikes@gmail.com>
2026-04-17 20:16:11 +0530

    practice

---
+++ practice.py
@@ -0,0 +1,10 @@
+# dictionary = {
+#     "table":["a piece of furniture","list of facts & figures"],
+#     "cat":"a small animal"
+# }
+# print(dictionary)
+
+classes = {"python","java","C++","python","javaScript","python","java","java","C++","C"}
+
+print(len(classes))
+
